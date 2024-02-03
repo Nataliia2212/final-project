5edc69f5b8bf91d07f2ec3ec3f9e74b3204e3ce4
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-02-03 23:40:46 +0200

    ...

--- src/js/favorites-block.js
+++ src/js/favorites-block.js
@@ -1,43 +1,131 @@
 console.log('1234')
+import axios from "axios";
+
+
+const results = [
+    {
+      _id: "64f26083f39b17a4",
+      bodyPart: "waist",
+      name: "air bike",
+      target: "abs",
+      burnedCalories: 312,
+      time: 3,
+    },
+   {
+      _id: "64",
+      bodyPart: "waist",
+      name: "air bike",
+      target: "abs",
+      burnedCalories: 312,
+      time: 3,
+    },
+   {
+      _id: "3a4",
+      bodyPart: "waist",
+      name: "air bike",
+      target: "abs",
+      burnedCalories: 312,
+      time: 3,
+    },
+   {
+      _id: "64f389465ae265a4",
+      bodyPart: "waist",
+      name: "air bike",
+      target: "abs",
+      burnedCalories: 312,
+      time: 3,
+    },
+  ]
 
 
 const favoritesRef = document.querySelector('.favorites-list');
 console.log(favoritesRef)
 
-const keyLS = ".favorites-exercises";
+const keyLS = "favorites-exercises";
 
 favoritesRef.addEventListener('click', onExercisesClick);
 
-function onExercisesClick(event) {
+async function onExercisesClick(event) {
+  
+    if (event.target.dataset.action === 'trash') {
+        console.log('YRA')
+    } else if (event.target.dataset.action === 'start') {
+       try {
+            const data = await search('64f389465ae26083f39b17a4');
+            console.log(data.bodyPart)
+       }
+       catch (error) {
+            console.log(error)
+        }
+    } else {
+        return
+    }
+    
+   
+   
 
-    console.log(event)
-    // const email = formElem.elements.email.value.trim();
-    // const message = formElem.elements.message.value.trim();
 
     // const data = { email, message }
     // // console.log(data)
     // saveToLS(keyLS, data);
 }
 
-// function saveToLS(key, info) {
-//     const zip = JSON.stringify(info)
-//     localStorage.setItem(key, zip)
-// }
+function loadFromLS(keyLS) {
+  const zip = localStorage.getItem(keyLS);
+  try {
+    const data = JSON.parse(zip);
+    return data;
+  } catch {
+    return zip;
+  }
+}
 
-// formElem.addEventListener('submit', onFormSubmit);
+// console.log(loadFromLS(keyLS))
 
-// function onFormSubmit(event) {
-//     event.preventDefault();
+function saveToLS(key, info) {
+    const zip = JSON.stringify(info)
+    localStorage.setItem(key, zip)
+}
+
+saveToLS(keyLS, results)
+
+
+function deleteExercise(id) {
+    const exercises = loadFromLS(keyLS)
+    console.log(exercises)
+    console.log('??????')
+ exercises.indexof(id)
+        
+exercises.map((element, index, array) => {
+  // Тіло колбек-функції
+});
+
+//    for (let i = 0; i < exercises.length; i++) {
+//         console.log(i)
+
+//         //  if (exercises[i]._id === id) {
+//         //      console.log('hgfdsdfghjk')
+//         //      console.log(exercises[i]._id)
+//         //         exercises[i].splice(i, 1);
+//         //         break;
+//         //     }
+//     }
+    console.log(exercises)
+        return exercises;
+}
+deleteExercise('64f389465ae265a4')
+// console.log(deleteExercise('64f389465ae265a4'))
+// console.log(loadFromLS(keyLS))
+
+
+
 
 //     const email = formElem.elements.email.value;
 //     const message = formElem.elements.message.value;
 
 //     const data = { email, message };
 
-//    if (!email || !message) {
-//        alert('All form fields must be filled in')
-//        return
-//    }
+
 
 //     console.log(data);
 
@@ -45,15 +133,8 @@ function onExercisesClick(event) {
 //      formElem.reset();
 // }
 
-// function loadFromLS(keyLS) {
-//   const zip = localStorage.getItem(keyLS);
-//   try {
-//     const data = JSON.parse(zip);
-//     return data;
-//   } catch {
-//     return zip;
-//   }
-// }
+
+
 
 // function restoreData() {
 //   const data = loadFromLS(keyLS) || {};
@@ -61,4 +142,15 @@ function onExercisesClick(event) {
 //   formElem.elements.message.value = data.message || '';
 // }
 
-// restoreData();
+
+
+
+
+// const search = async (id) => {
+//     try {
+//         const request = await axios.get(`https://energyflow.b.goit.study/api/exercises/${id}`)
+//         return request.data
+//     } catch (error) {
+// 		return error.message
+// 	}    
+// }
