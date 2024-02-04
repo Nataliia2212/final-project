2cf355cb60e54e99945089af3614271d61837e85
Baromental <suzdalcevmaksym@gmail.com>
2024-02-04 21:31:09 +0200

    done js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,1 +1,23 @@
-console.log('1234')
+import axios from 'axios';
+
+function submitForm() {
+  const email = document.getElementById('email').value;
+
+  const data = {
+    email: email
+  };
+
+  axios.post('https://energyflow.b.goit.study/api/subscription', data)
+    .then(response => {
+      console.log(response.data);
+      alert('Замовлення успішно відправлено!');
+    })
+    .catch(error => {
+      console.error('Помилка відправлення замовлення:', error);
+      alert('Сталася помилка. Будь ласка, спробуйте ще раз.');
+    });
+}
+
+const sendButton = document.getElementById('sendButton');
+
+sendButton.addEventListener('click', submitForm);
