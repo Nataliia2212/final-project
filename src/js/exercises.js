ec1db46044ef1399747f2dc8fc92b366a343ceb3
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2024-02-04 10:17:50 -0700

    markup and js withought paginnation

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -28,12 +28,36 @@ const exercisesAPI = new ExercisesAPI();
 
 const gallery = document.querySelector('.gallery-list');
 const filterBTN = document.querySelector('.filter-list');
-const btn = document.querySelector('.filter-button');
+const btn = document.querySelectorAll('.filter-button');
+const title = document.querySelector('.exercises-title');
+let elem = document.getElementsByClassName('active-btn');
+const pages = document.querySelector('.page-number-list');
+
+async function defaultSettings() {
+  title.textContent = 'Exercises';
+  exercisesAPI.filter = 'Muscles';
+  const result = await exercisesAPI.fetchImages();
+  console.log(result);
+  const markup = imagesTemplate(result.results);
+  if (elem) {
+    gallery.innerHTML = markup;
+  }
+  const pageMarkup = pagesTemplate(result.totalPages);
+  pages.innerHTML = pageMarkup;
+}
+defaultSettings();
 
 filterBTN.addEventListener('click', async function (e) {
-  btn.classList.add('active-btn');
-  console.log(btn);
-
+  btn.forEach(button => {
+    if (
+      button.getElementsByClassName('active-btn') ===
+      e.target.getElementsByClassName('active-btn')
+    ) {
+      button.classList.add('active-btn');
+    } else {
+      button.classList.remove('active-btn');
+    }
+  });
   let target = e.target.name;
 
   if (target === 'muscles') {
@@ -47,29 +71,42 @@ filterBTN.addEventListener('click', async function (e) {
   }
   const result = await exercisesAPI.fetchImages();
   const markup = imagesTemplate(result.results);
-  let elem = document.getElementsByClassName('active-btn');
-  console.log(elem);
   if (elem) {
     gallery.innerHTML = markup;
   }
 });
 
 function imgTemplate({ filter, imgUrl, name }) {
+  let modName = name[0].toUpperCase() + name.slice(1);
+  let modFilter = filter[0].toUpperCase() + filter.slice(1);
+
   return `<li class="gallery-item">
         <div class="img-container">
           <img
             class="exercises-img"
             src=${imgUrl}
-            alt=${name}
+            alt=${modName}
           />
         </div>
         <div class="muscles">
-          <span class="muscles-group">${name}</span>
-          <span class="muscles-group-name">${filter}</span>
+          <span class="muscles-group">${modName}</span>
+          <span class="muscles-group-name">${modFilter}</span>
         </div>
       </li>`;
 }
 
 function imagesTemplate(images) {
   return images.map(imgTemplate).join('');
+}
+
+function pageTemplate(pageNumber) {
+  return `<li class="page-number-item">${pageNumber}</li>`;
+}
+
+function pagesTemplate(total) {
+  const pagesArray = [];
+  for (let i = 1; i <= total; i++) {
+    pagesArray.push(pageTemplate(i));
+  }
+  return pagesArray.join('');
 }
