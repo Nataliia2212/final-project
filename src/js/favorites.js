1eca5701151d3508655e89f94d3975b6b161cf26
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-02-06 15:28:12 +0100

    modal

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -3,3 +3,9 @@ import './quote';
 import './favorites-block';
 import './modal-exersises';
 import './modal-feedback';
+
+const favorPageEl = document.querySelector('.js-link-favor');
+favorPageEl.classList.add('link-home-favor');
+
+const favorPageMob = document.querySelector('.js-link-favor-mobail');
+favorPageMob.classList.add('link-home-favor');
